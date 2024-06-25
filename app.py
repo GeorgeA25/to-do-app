dd542c5e073b6a6826b7285bba209403bb2b4b1f
unknown <GeorgeA25@users.noreply.github.com>
2024-06-25 13:43:18 +0100

    rechanged line 58 to 56

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ from website import create_app
 #     with app.app_context():
 #         db.create_all()
 #     app.run(debug=True, port=8000)
-
+app= create_app() 
 if __name__== "__main__":
-    app= create_app() 
+    
     app.run(debug=True)
