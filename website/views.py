8f578be0866df3894f6b8751068d1d9fc6680edd
unknown <GeorgeA25@users.noreply.github.com>
2024-06-25 13:20:06 +0100

    first commit

---
+++ website/views.py
@@ -0,0 +1,46 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from .moduels import Todo
+from . import db
+
+my_view= Blueprint("my_view", __name__)
+
+@my_view.route("/")
+def home():
+    todo_list= Todo.query.all()
+    message=request.args.get('message', None)
+    return render_template("index.html", todo_list=todo_list, message=message)
+
+@my_view.route("/update/<todo_id>")
+def update(todo_id):
+    todo=Todo.query.filter_by(id=todo_id).first()
+    todo.complete= not todo.complete
+    db.session.commit()
+    return redirect(url_for("my_view.home"))
+
+@my_view.route("/delete/<todo_id>")
+def delete(todo_id):
+    todo=Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("my_view.home"))
+
+@my_view.route("/add", methods=["POST"])
+def add():
+    try:
+     task = request.form.get("task")
+     new_todo = Todo(task=task)
+     db.session.add(new_todo)
+     db.session.commit()
+     return redirect(url_for("my_view.home"))
+    except:
+        message= ("There was an error adding your task. Please try again")
+        return redirect(url_for("my_view.home", message=message))
+
+
+@my_view.route("/edit/<todo_id>", methods=["POST"])
+def edit(todo_id):
+     todo=Todo.query.filter_by(id=todo_id).first()
+     new_title=request.form.get("edit_task")
+     todo.task= new_title
+     db.session.commit()
+     return redirect(url_for("my_view.home"))
